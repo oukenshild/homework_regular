bba46827e11fb91e5ae259065bbf6a74ad3e9be9
Danil <danililukhin@yandex.ru>
2023-02-03 13:22:26 +0500

    Code updated

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
 from pprint import pprint
-# читаем адресную книгу в формате CSV в список contacts_list
 import csv
 import re
 
-with open("C:\\Users\\Данил Илюхин\\PycharmProjects\\Homework_regular\\phonebook_raw.csv", encoding="utf-8") as in_file:
-    rows = csv.reader(in_file, delimiter=",")
-    contacts_list = list(rows)
-    contacts_list_updated = []
 
-
-# TODO 1: выполните пункты 1-3 ДЗ
 def names_moving():
     name_pattern = r'([А-Я])'
     name_substitution = r' \1'
@@ -40,31 +33,32 @@ def phone_number_formatting():
 
 
 def duplicates_combining():
+    contact_list = {}
     for column in contacts_list[1:]:
         last_name = column[0]
-        first_name = column[1]
-        for contact in contacts_list[1:]:
-            new_last_name = contact[0]
-            new_first_name = contact[1]
-            if last_name == new_last_name and first_name == new_first_name:
-                for item in range(2, 7):
-                    if contact[item] == '':
-                        contact[item] = column[item]
+        if last_name not in contact_list:
+            contact_list[last_name] = column
+        else:
+            for ind, item in enumerate(contact_list[last_name]):
+                if item == '':
+                    contact_list[last_name][ind] = column[ind]
 
-    for contact in contacts_list:
-        if contact not in contacts_list_updated:
-            contacts_list_updated.append(contact)
+    for last_name, contact in contact_list.items():
+        for contacts in contact:
+            if contact not in contacts_list_updated:
+                contacts_list_updated.append(contact)
     return contacts_list_updated
 
 
 if __name__ == '__main__':
-    names_moving()
-    phone_number_formatting()
-    duplicates_combining()
-
-# TODO 2: сохраните получившиеся данные в другой файл
-# код для записи файла в формате CSV
-with open("C:\\Users\\Данил Илюхин\\PycharmProjects\\Homework_regular\\phonebook.csv", "w") as out_file:
-    datawriter = csv.writer(out_file, delimiter=',')
-    datawriter.writerows(contacts_list_updated)
-pprint(contacts_list_updated)
+    with open("phonebook_raw.csv", encoding="utf-8") as in_file:
+        rows = csv.reader(in_file, delimiter=",")
+        contacts_list = list(rows)
+        contacts_list_updated = []
+        names_moving()
+        phone_number_formatting()
+        duplicates_combining()
+    with open("phonebook.csv", "w") as out_file:
+        datawriter = csv.writer(out_file, delimiter=',')
+        datawriter.writerows(contacts_list_updated)
+    pprint(contacts_list_updated)
